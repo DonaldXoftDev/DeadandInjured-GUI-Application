3f2ff0183f562be0570f9d488e594f379950e7f1
Donald Okonkwo <chibuezeokon1007@gmail.com>
2025-12-07 23:15:12 +0100

    created the base player_model class

--- backend model/player_model.py
+++ backend model/player_model.py
@@ -12,17 +12,6 @@ class PlayerModel:
         self.feedback_history = []
         self.is_human = True
 
-    def validate_unique_code(self, input_string: str) -> List:
-        clean_string = input_string.strip()
-        if len(clean_string) != 4 or not clean_string.isdigit():
-            return []
-
-        if len(set(clean_string)) < 4:
-            return []
-
-        return [int(n) for n in clean_string]
-
-
 
 
 
