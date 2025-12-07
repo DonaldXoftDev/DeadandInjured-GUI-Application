f947c801d22141e3e32d27b0db805810647a6919
Donald Okonkwo <chibuezeokon1007@gmail.com>
2025-12-07 23:14:47 +0100

    refactored this file

--- backend model/feedback_mechanism.py
+++ backend model/feedback_mechanism.py
@@ -1,17 +1,17 @@
 from typing import Dict
 
 class Feedback:
-    def __init__(self, comp_result:Dict):
-        self.comp_result = comp_result
+    def __init__(self, feedback_data:Dict):
+        self.feedback_data = feedback_data
 
     def feedback_result(self) -> str:
-        if self.comp_result['dead'] == 4 and self.comp_result['injured'] == 0:
+        if self.feedback_data['dead'] == 4 and self.feedback_data['injured'] == 0:
             return 'All dead'
 
-        if self.comp_result['dead'] == 0 and self.comp_result['injured'] == 0:
+        if self.feedback_data['dead'] == 0 and self.feedback_data['injured'] == 0:
             return 'No matches'
 
         else:
-            return f'{self.comp_result['dead']}dead and {self.comp_result['injured']}inj'
+            return f'{self.feedback_data['dead']}dead and {self.feedback_data['injured']}inj'
 
 
