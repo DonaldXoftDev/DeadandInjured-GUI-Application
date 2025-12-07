8b8908850c0488efae44018ce65424bbfc77febf
Donald Okonkwo <chibuezeokon1007@gmail.com>
2025-12-07 23:14:26 +0100

    initialised a computer_player.py to inherit from base player_model.py class

--- backend model/computer_player.py
+++ backend model/computer_player.py
@@ -1,42 +1,27 @@
 import random
-from player import Player
+from player_model import PlayerModel
 from itertools import permutations
 
 
-class ComputerPlayer(Player):
+class ComputerPlayer(PlayerModel):
     def __init__(self):
         super().__init__(name='Computer')
         self.possible_pin_list = [list(item) for item in permutations(range(0, 10), 4)]
 
     def computer_pin(self):
-        self.pin.clear()
-        computer_pin = random.choice(self.possible_pin_list)
-        self.pin.append(random.choice(computer_pin))
-        return computer_pin
+        pin = random.choice(self.possible_pin_list)
+        self.pin = pin
+        return pin
+
 
     def computer_guess(self):
         self.guess.clear()
-        computer_guess = random.choice(self.possible_pin_list)
-        self.guess.append(computer_guess)
-        return computer_guess
-
-    def computer_guessing_strategy(self, pin):
-        self.guess = self.computer_guess()
-        print(self.guess.copy())
-        feedback_dead, feedback_inj= self.compare_guesses(pin)
-
-        for possible_pin in self.possible_pin_list:
-            possible_dead, possible_inj= self.compare_guesses(possible_pin)
-            if feedback_dead == possible_dead and feedback_inj == possible_inj:
-                continue
-            else:
-                self.possible_pin_list.remove(possible_pin)
+        comp_guess = random.choice(self.possible_pin_list)
+        self.guess = comp_guess
+        return comp_guess
 
 
-computer = ComputerPlayer()
-user_pin = [8, 1, 2, 3]
-computer.computer_guessing_strategy(user_pin)
-print(computer.display_feedback_message())
-
+def computer_guess_strategy():
+    pass
 
 
